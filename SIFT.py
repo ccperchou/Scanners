f56f1a3d2c7946763747abdb32a3503b0acf0342
ccperchou <clement.perchais@live.fr>
2023-05-31 16:01:11 +0200

    SIFT

--- SIFT.py
+++ SIFT.py
@@ -1,50 +1,89 @@
 import cv2
 import os
 from datetime import datetime
-import time 
-#Script de capture vidéo avec la camera
-# Pictures will be stored in the the folder captures with the format name Year month day Hour minutes secondes 
-def capture_image(number_pic ):
-    # Créer un dossier pour enregistrer les images s'il n'existe pas déjà
-    if not os.path.exists("captures"):
-        os.makedirs("captures")
+import time
+import numpy as np 
+# Methode Script 
+# Input : Pic n°1 and Pic n° 2
+# Output : Commun points and marker
 
-    # Ouvrir la webcam
-    cap = cv2.VideoCapture(0)
+# function  find_matching points for two pics
+def find_matching_points(image1, image2):
+    # Chargement des images
+    img1 = cv2.imread(image1, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(image2, cv2.IMREAD_GRAYSCALE)
 
-    # Vérifier si la webcam est ouverte correctement
-    if not cap.isOpened():
-        print("Impossible d'ouvrir la webcam.")
-        return
+    # Création de l'extracteur de points d'intérêt
+    sift = cv2.SIFT_create()
 
-    while True:
-        # Lire une image depuis la webcam
-        ret, frame = cap.read()
+    # Détection des points d'intérêt et de leurs descripteurs dans les deux images
+    keypoints1, descriptors1 = sift.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(img2, None)
 
-        # Afficher l'image capturée
-        #cv2.imshow("Webcam", frame
-        # Vérifier si l'utilisateur a appuyé sur la touche 's' pour capturer l'image
-        for i  in range(0,number_pic ):
-            # Obtenir la date et l'heure actuelles
-            now = datetime.now()
-            time.sleep(0.5)
-            timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    # Création du matcher de descripteurs
+    matcher = cv2.BFMatcher(cv2.NORM_L2)
 
-            # Créer le nom de fichier en utilisant la date et l'heure
-            filename = f"captures/capture_{timestamp}.jpg"
+    # Recherche des correspondances entre les descripteurs
+    matches = matcher.match(descriptors1, descriptors2)
 
-            # Enregistrer l'image capturée
-            cv2.imwrite(filename, frame)
+    # Trie les correspondances en fonction de leur similarité
+    matches = sorted(matches, key=lambda x: x.distance)
 
-            print(f"Image capturée",i," : {filename}")
-            i=i+1
-		
-        # Vérifier si l'utilisateur a appuyé sur la touche 'q' pour quitter
-        break
+    # Sélectionne les meilleures correspondances (20% des correspondances avec la plus petite distance)
+    num_good_matches = int(len(matches) * 0.2)
+    good_matches = matches[:num_good_matches]
 
-    # Fermer la webcam et détruire les fenêtres OpenCV
-    cap.release()
-    cv2.destroyAllWindows()
+    # Récupère les points d'intérêt correspondants dans les deux images
+    points1 = np.float32([keypoints1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    points2 = np.float32([keypoints2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-# Appeler la fonction pour capturer des images
-capture_image(40)
+    return points1, points2
+
+def find_matching_points(images):
+    # Chargement des images et création de l'extracteur de points d'intérêt
+    sift = cv2.SIFT_create()
+    keypoints_list = []
+    descriptors_list = []
+
+    # Extraction des points d'intérêt et des descripteurs pour chaque image
+    for image in images:
+        img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        keypoints, descriptors = sift.detectAndCompute(img, None)
+        keypoints_list.append(keypoints)
+        descriptors_list.append(descriptors)
+
+    matcher = cv2.BFMatcher(cv2.NORM_L2)
+
+    common_points = []
+    for i in range(len(images)-1):
+        # Recherche des correspondances entre les descripteurs de l'image i et l'image suivante (i+1)
+        matches = matcher.match(descriptors_list[i], descriptors_list[i+1])
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        # Sélection des meilleures correspondances
+        num_good_matches = int(len(matches) * 0.2)
+        good_matches = matches[:num_good_matches]
+
+        # Récupération des points d'intérêt correspondants
+        points1 = np.float32([keypoints_list[i][m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        points2 = np.float32([keypoints_list[i+1][m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+        common_points.append((points1, points2))
+
+    return common_points
+
+# Exemple d'utilisation
+image_paths = ['chemin/vers/image1.jpg', 'chemin/vers/image2.jpg', 'chemin/vers/image3.jpg']
+
+common_points = find_matching_points(image_paths)
+
+# Affichage des points d'intérêt correspondants sur les paires d'images
+for i, (points1, points2) in enumerate(common_points):
+    img1 = cv2.imread(image_paths[i])
+    img2 = cv2.imread(image_paths[i+1])
+    matched_img = cv2.drawMatches(img1, keypoints_list[i], img2, keypoints_list[i+1], good_matches, None)
+
+    cv2.imshow('Matches', matched_img)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
